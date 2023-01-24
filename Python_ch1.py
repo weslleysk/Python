3fa4f000799a619fb17706d0f4e32ba11d44e7a4
claustrophobic <claustrophobic@LAPTOP-P6BM3VOK>
2023-01-24 08:42:02 -0800

    Chapter 1 - end.

--- Python_ch1.py
+++ Python_ch1.py
@@ -29,3 +29,21 @@ print(math.ceil(1.2))
 print(cmath.sqrt(9))
 print(cmath.sqrt(-1))
 
+#Strings
+x = str("Claustrophobic hell is an ")
+y = input()
+print(x + y)
+
+x = 12345
+y = str(x)
+print(y[0], y[1], y[2])
+print(len(y))
+
+print("\\n is a \n new line")
+
+path = "C:\\nowhere"
+print(path)
+print(r'C:\Program Files')
+
+print(len("String".encode("UTF-8")))
+print(len("String".encode("UTF-32")))
