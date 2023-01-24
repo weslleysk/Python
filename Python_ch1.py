9283511d740fee116469c923e7d7e3636e71a2e8
claustrophobic <claustrophobic@LAPTOP-P6BM3VOK>
2023-01-24 07:33:30 -0800

    Add project files.

---
+++ Python_ch1.py
@@ -0,0 +1,31 @@
+#Operations
+x = 10
+y = 2
+
+print(x+y)
+print(x-y)
+
+print(x/y)
+print(x//y)
+print(x%y)
+
+print(x**y)
+print(pow(x, y))
+
+print(abs(-x))
+
+z = 2
+w = 3
+print(z//w)
+print(round(z/w))
+
+#Modules - import module module.function
+import math
+import cmath
+
+print(math.floor(1.9))
+print(math.ceil(1.2))
+
+print(cmath.sqrt(9))
+print(cmath.sqrt(-1))
+
