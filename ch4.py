ddf73d6a427175ddeae5ea7576995b6e5e38a754
weslleysk <weslleysk@outlook.com>
2023-01-25 10:10:20 -0800

    Ch4 - Dictionary database

--- ch4.py
+++ ch4.py
@@ -2,7 +2,7 @@
 
 # dictionary = {"key":"value", "key2":"value2", "key3":"value3"}
 
-#dict() len() del d[key] d[key] = value
+#dict() len() del d[key] d[key] = value   d[key1][key2]...
 
 items = [('key1', 'value1'), ('key2', 'value2')]
 d = dict(items)
@@ -26,4 +26,32 @@ d["Freud"] = "Biography"
 print(d)
 
 #Simple Database
+
+oeuvre = {
+
+    "Soren" : 
+        {
+            "book":"Concept of Anxiety",
+            "quantity":200
+        },
+    "Huxley" :
+        {
+            "book":"Devils of Loudun",
+            "quantity":100
+        },
+    "Freud" :
+        {
+            "book":"Biography",
+            "quantity":250
+        }
+
+    }
+
+author = input("author: ")
+request = input("Book (b) or Quantity (q)?")
+if request == "b" : key = "book"
+if request == "q" : key = "quantity"
+
+if author in oeuvre : print("{} : {}".format(author, oeuvre[author][key] ))
+
 
