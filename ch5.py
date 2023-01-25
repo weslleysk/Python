dc2503ca0d6e7d63bbc25da121fd8615096bb248
weslleysk <weslleysk@outlook.com>
2023-01-25 15:33:43 -0800

    Ch5 - update

--- ch5.py
+++ ch5.py
@@ -1,3 +1,25 @@
 #Chapter 5 - Conditionals, Loops, and some other Statements
 
+#print
+
+print("Soren", "Kierkegaard")
+
+x = "Aldous Huxley"
+y = "Sigmund Freud"
+z = "Soren Kierkegaard"
+
+print(x, y, z)
+
+print("Same ", end="")
+print("Line")
+
+#module
+
+#import module  from module import function, function2, function3   from module import *
+
+
+
+
+
+
 
