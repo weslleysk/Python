60934fb7479bb95f1520896aa6fc1741c6d4ca5c
weslleysk <weslleysk@outlook.com>
2023-01-27 19:07:24 -0800

    Ch6 - update

--- ch6.py
+++ ch6.py
@@ -3,7 +3,14 @@
 # functions
 
 def f(author):
-    print("My favorite author is ", author)
-f("Kierkegaard")
+    return "My favorite author is " + author
+print(f("Kierkegaard"))
+
+def fibs(num):
+    result = [0, 1]
+    for i in range(num-2):
+        result.append(result[-2] + result[-1])
+    return result
+print(fibs(10))
 
 
