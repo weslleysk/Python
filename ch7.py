c0184d980b349c7b83f387abc50368c7bd6056f5
weslleysk <weslleysk@outlook.com>
2023-02-05 06:12:02 -0800

    ch7 - end

--- ch7.py
+++ ch7.py
@@ -34,3 +34,57 @@ print("")
 
 author.name = "Huxley"
 author.greet()
+
+print("")
+
+
+class Bird():
+    song = "~~~~"
+    def sing(self):
+        print(self.song)
+
+bird = Bird()
+bird.sing()
+birdsong = bird.sing
+birdsong()
+
+
+class Security:
+    def __private(self):
+        print("Private")
+
+    def public(self):
+        print("Public")
+        self.__private()
+
+var = Security()
+var.public()
+
+
+
+print("")
+
+
+
+def f(x): return x * x
+print(f(2))
+
+y = lambda x: x * x
+print(y(3))
+
+
+
+print("")
+
+
+
+class MemberCounter:
+    members = 0
+    def init(self):
+        MemberCounter.members += 1
+
+m1 = MemberCounter()
+m1.init()
+m2 = MemberCounter()
+m2.init()
+print(MemberCounter.members)
