40554a913430064d5a624bc209f300699b515f9a
weslleysk <weslleysk@outlook.com>
2023-02-06 16:18:53 -0800

    Ch8 - end

--- ch8.py
+++ ch8.py
@@ -3,7 +3,33 @@ try:
     y = int(input('Enter the second number: '))
     print(x / y)
 except Exception: 
-    print("The second number can't be zero!")
-
+    print("Error")
+"""except:
+    print("Error")
+"""
 ## AttributeError, OSError, etc
+
+while True:
+    try:
+        x = int(input('Enter the first number: '))
+        y = int(input('Enter the second number: '))
+        print(x / y)
+    except:
+        print('Invalid input. Please try again.')
+    else:
+        break   #it will only break when both numbers are correct
+
+database = {"author":"soren", "book":"concept of anxiety"}
+
+def description(d):
+    print(d["author"])
+    print(d["book"])
+    try:
+        print(d["genre"])
+    except:
+        print("Genre does not exist in dictionary")                                              
+    """if "genre" in d : print(d["genre"])
+    else : print("Genre does not exist in dictionary")"""
+
+description(database)
 
