7f62c4deea701c927c545724bae3c4430028e700
weslleysk <weslleysk@outlook.com>
2023-02-12 13:37:50 -0800

    Ch9 - end

--- ch9.py
+++ ch9.py
@@ -1,1 +1,23 @@
+# Class Constructor __init__
 
+class C:
+    def __init__(self):
+        self.somevar = 10
+
+x = C()
+print(x.somevar)
+
+class Bird:
+    def __init__(self):
+        self.hungry = True
+    def eat(self):
+        if self.hungry:
+            print('Aaaah ...')
+            self.hungry = False
+        else:
+            print('No, thanks!')
+
+x = Bird()
+x.eat()
+#x = Bird()
+x.eat()
