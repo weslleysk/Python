5a9c30fb74d35176f1eb74e3f5d7971252b4da81
weslleysk <weslleysk@outlook.com>
2023-02-26 02:03:43 -0800

    Study more, you bastard.

---
+++ ch10.py
@@ -0,0 +1,14 @@
+# Modules
+
+import math
+print(math.sin(9))
+
+def hello():
+    print("Comment allez vous? - study more french, you bastard.")
+
+#CMD : cd C:\Users\claus\OneDrive\Desktop\Work\Python 
+#python
+#import ch10
+#ch10.hello()
+
+
